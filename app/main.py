bd58274e64f4a48610ccba421a05f1eadeabebbf
Kacper Syrnik <kacper.syrnik1@gmail.com>
2023-05-17 22:12:09 +0200

    Add CORS configuration

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.routers import auth_router
 
@@ -6,3 +7,12 @@ app = FastAPI()
 
 app.include_router(auth_router.router)
 
+
+# Configure CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
